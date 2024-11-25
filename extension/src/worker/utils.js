d87824a196b949c3be84c265b6a8741069e14fad
seojinseojin <001106ksj@gmail.com>
2024-11-25 19:42:00 +0900

    [extension] modify utils.js

--- extension/src/worker/utils.js
+++ extension/src/worker/utils.js
@@ -1,17 +1,19 @@
 export const calcScore = (allVersionWithProperties, targetProperties) =>
   allVersionWithProperties
     .map((versionWithProperties) => {
+      const allProperties = new Set([
+        ...versionWithProperties.directChildren,
+        ...targetProperties,
+      ]);
       const matches = targetProperties.filter((tp) =>
         versionWithProperties.directChildren.includes(tp)
       );
       return {
         src: versionWithProperties.src,
         // true positives / all
-        score: (
-          matches.length / versionWithProperties.directChildren.length
-        ).toFixed(5),
+        score: (matches.length / allProperties.size).toFixed(5),
         truePositivesNum: matches.length,
-        allNum: versionWithProperties.directChildren.length,
+        allNum: allProperties.size,
       };
     })
     .sort((a, b) => b.score - a.score);
