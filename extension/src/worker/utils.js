7abc9066c89a024b3fc84a9e0bb23b2b7348310a
seojinseojin <001106ksj@gmail.com>
2024-11-26 15:18:24 +0900

    [extension] revise score calc logic - do not consider external sub properties

--- extension/src/worker/utils.js
+++ extension/src/worker/utils.js
@@ -3,39 +3,40 @@ export const calcScore = (libData, windowProps) => {
   for (const name in libData) {
     const { src, tree } = libData[name];
     const versions = Object.keys(src);
-    const indexOf = {};
+    const all = {};
     const matched = {};
     versions.forEach((version, i) => {
-      indexOf[version] = i;
+      all[version] = 0;
       matched[version] = 0;
     });
-    let all = 0;
     for (const prop in tree) {
       const subProps = tree[prop];
-      all += Object.keys(subProps).length;
       const subWindowProps = new Set(windowProps[prop]);
       for (const sub in subProps) {
         const ranges = subProps[sub];
-        const has = subWindowProps.has(sub);
-        let delta = 1;
-        if (!has) {
-          for (const version in matched) matched[version]++;
-          delta = -1;
-        }
-        if (subWindowProps.has(sub)) {
-          for (const [fromIndex, toIndex] of ranges) {
-            for (let i = fromIndex; i <= toIndex; i++) {
-              const version = versions[i];
-              matched[version] += delta;
+        for (const [fromIndex, toIndex] of ranges) {
+          for (let i = fromIndex; i <= toIndex; i++) {
+            const version = versions[i];
+            all[version] += 1;
+            if (subWindowProps.has(sub)) {
+              matched[version] += 1;
             }
           }
         }
       }
     }
+
     for (const version in matched) {
       const count = matched[version];
-      const score = (count / all).toFixed(5);
-      scores.push({ name, version, count, all, score, src: src[version] });
+      const score = (count / all[version]).toFixed(5);
+      scores.push({
+        name,
+        version,
+        count,
+        all: all[version],
+        score,
+        src: src[version],
+      });
     }
   }
   scores.sort((a, b) => b.score - a.score);
