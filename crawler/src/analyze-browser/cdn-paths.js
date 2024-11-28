eabcd757be40d935051fb6024020cfa0699d3394
seojinseojin <001106ksj@gmail.com>
2024-11-28 15:31:09 +0900

    [crawler] do not consider nodejs files from cdnjs

--- crawler/src/analyze-browser/cdn-paths.js
+++ crawler/src/analyze-browser/cdn-paths.js
@@ -19,6 +19,20 @@ export const parseCdnPath = (url) => {
   };
 };
 
+const fetchAndCheckValidCdnScript = async (url) => {
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const scriptContent = response.data;
+      const isNodeScript = scriptContent.includes(`require('fs')`);
+      return !isNodeScript;
+    }
+  } catch {
+    return false;
+  }
+  return false;
+};
+
 // (string[]) => Record<string,{version:string, src:string, idx:number}[]>
 export const getCdnPaths = async (libraries) => {
   const versionStrings = {};
@@ -28,39 +42,39 @@ export const getCdnPaths = async (libraries) => {
       const libInfo = getLibInfo(lib);
       if (!libInfo) return;
 
-      const promises = libInfo.versions.map(async (version, idx) => {
+      const promises = libInfo.versions.map(async (version) => {
         const fileName = libInfo.filename;
 
         try {
-          const isValidFileName = await axios.get(
+          const isValidCdnScript = await fetchAndCheckValidCdnScript(
             cdnTemplate(lib, version, fileName)
           );
-
-          if (isValidFileName.status === 200) {
+          if (isValidCdnScript)
             return {
               version,
               src: cdnTemplate(lib, version, fileName),
-              idx,
             };
-          }
         } catch (error) {}
 
         try {
           const versionInfo = await fetchVersionAssets(lib, version);
           const heuristicFileName = versionInfo.find((f) => f.endsWith('.js'));
-          if (heuristicFileName) {
+          const isValidCdnScript = await fetchAndCheckValidCdnScript(
+            cdnTemplate(lib, version, heuristicFileName)
+          );
+          if (isValidCdnScript)
             return {
               version,
               src: cdnTemplate(lib, version, heuristicFileName),
-              idx,
             };
-          }
         } catch (error) {}
 
         return null;
       });
 
-      const resolvedVersions = (await Promise.all(promises)).filter(Boolean);
+      const resolvedVersions = (await Promise.all(promises))
+        .filter(Boolean)
+        .map((v, idx) => ({ ...v, idx }));
       versionStrings[lib] = resolvedVersions;
     })
   );
