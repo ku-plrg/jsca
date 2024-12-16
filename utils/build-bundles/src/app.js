06242cd0397ee5fe6c3c598ee5a1d1a778f4ceef
seojinseojin <001106ksj@gmail.com>
2024-12-16 20:36:48 +0900

    [utils][build-bundles] enhance example

--- utils/build-bundles/src/app.js
+++ utils/build-bundles/src/app.js
@@ -1,7 +1,5 @@
 import is_number from 'is-number';
 
 document.addEventListener('DOMContentLoaded', () => {
-  $('div#root').html(
-    `<h1>jquery works well</h1><h2>is 4 number? ${is_number(4)}</h2>`
-  );
+  console.log(is_number);
 });
