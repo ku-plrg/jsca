625bb15e5a2af87448b9c2111ba1213e21784697
김서진 <001106ksj@gmail.com>
2025-01-07 14:07:18 +0900

    [utils][static-analysis] migrate tree-comparator to typescript (#3)

--- utils/static-analysis/src/compare/tree-comparator.ts
+++ utils/static-analysis/src/compare/tree-comparator.ts
@@ -1,51 +1,66 @@
-import exp from 'constants';
-const options = {};
+import { proptree, proptreeNode } from '../utils/types';
 
-const hasSameMembers = (arr1, arr2) => {
+type Option = {
+  node_type: boolean;
+  order: boolean;
+  compare_other_props: boolean;
+};
+
+const hasSameMembers = (arr1: string[], arr2: string[]) => {
   const set1 = new Set(arr1);
   const set2 = new Set(arr2);
-  return (
-    set1.length === set2.length && [...set1].every((value) => set2.has(value))
-  );
+  return set1.size === set2.size && [...set1].every((value) => set2.has(value));
 };
-const isEqualObject = (obj1, obj2) => {
+
+const isEqualObject = (
+  obj1: Record<string, any>,
+  obj2: Record<string, any>
+) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   if (keys1.length !== keys2.length) return false;
   return keys1.every((key) => obj1[key] === obj2[key]);
 };
 
-function compare(tree1, tree2, options) {
-  const sortArray = (arr) => [...(arr || [])].sort();
+function compare(func1: proptree, func2: proptree, options: Option) {
+  const sortArray = (arr: any[]) => [...(arr || [])].sort();
 
-  const compareOtherProps = (t1, t2) => {
+  const compareOtherProps = (t1: proptreeNode, t2: proptreeNode) => {
     const { literals: l1 = [], ...op1 } = t1.otherProps ?? {};
     const { literals: l2 = [], ...op2 } = t2.otherProps ?? {};
     return hasSameMembers(l1, l2) && isEqualObject(op1, op2);
   };
 
-  const compareProps = (t1, t2) => {
+  const compareProps = (t1: proptreeNode, t2: proptreeNode) => {
     const props1 = sortArray(t1.props);
     const props2 = sortArray(t2.props);
     if (props1.length !== props2.length) return false;
     return props1.every((prop, i) => prop === props2[i]);
   };
 
-  const getChild = (node) => {
-    const paths = node?.paths || {};
-    return {
-      left: paths.true || paths.left,
-      right: paths.false || paths.right,
-    };
+  const getChild = (node: proptreeNode) => {
+    switch (node.type) {
+      case 'if':
+      case 'conditional':
+        return {
+          left: node.paths.true,
+          right: node.paths.false,
+        };
+      case 'logical':
+        return {
+          left: node.paths.left,
+          right: node.paths.right,
+        };
+    }
   };
 
-  const comparePathNodes = (t1, t2) => {
+  const comparePathNodes = (t1: proptreeNode, t2: proptreeNode) => {
     if (!options.node_type && t1.type !== t2.type) return false;
 
     // if (t1.type === 'logical' && t1.operator !== t2.operator) return false;
 
-    const { left: left1, right: right1 } = getChild(t1, 'left');
-    const { left: left2, right: right2 } = getChild(t2, 'left');
+    const { left: left1, right: right1 } = getChild(t1);
+    const { left: left2, right: right2 } = getChild(t2);
 
     return options.order
       ? (compareTree(left1, left2) && compareTree(right1, right2)) ||
@@ -53,7 +68,7 @@ function compare(tree1, tree2, options) {
       : compareTree(left1, left2) && compareTree(right1, right2);
   };
 
-  const compareTree = (t1, t2) => {
+  const compareTree = (t1: proptreeNode, t2: proptreeNode) => {
     if (!t1 || !t2) return t1 === t2;
 
     if (!compareProps(t1, t2)) return false;
@@ -76,7 +91,7 @@ function compare(tree1, tree2, options) {
     return true;
   };
 
-  return compareTree(tree1, tree2);
+  return compareTree(func1.tree, func2.tree);
 }
 
 export default compare;
