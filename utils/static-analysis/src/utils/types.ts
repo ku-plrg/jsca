1555b26013b8f52b72d8c49d2709ed1d335153d5
seojinseojin <001106ksj@gmail.com>
2025-01-10 17:57:34 +0900

    [utils][static-analysis] refactor type of irnode

--- utils/static-analysis/src/utils/types.ts
+++ utils/static-analysis/src/utils/types.ts
@@ -89,51 +89,51 @@ export enum IRInst {
   DO_WHILE,
 }
 
-export interface IRNode {
+export interface IRNodeBase {
   type: IRInst;
 }
 
-export interface EmptyNode extends IRNode {
+export interface EmptyNode extends IRNodeBase {
   type: IRInst.EMPTY;
 }
 
-export interface BlockNode extends IRNode {
+export interface BlockNode extends IRNodeBase {
   type: IRInst.BLOCK;
 }
 
-export interface SeqNode extends IRNode {
+export interface SeqNode extends IRNodeBase {
   type: IRInst.SEQ;
   left: IRNode;
   right: IRNode;
 }
 
-export interface UpdatePropNode extends IRNode {
+export interface UpdatePropNode extends IRNodeBase {
   type: IRInst.UPDATE_PROP;
   left: PropNode;
-  right: BlockNode;
+  right: IRNode;
 }
 
-export interface PropNode extends IRNode {
+export interface PropNode extends IRNodeBase {
   type: IRInst.PROP;
   id: string;
   object: BlockNode;
 }
 
-export interface CondNode extends IRNode {
+export interface CondNode extends IRNodeBase {
   type: IRInst.COND;
   test: BlockNode;
   true: IRNode;
   false: IRNode;
 }
 
-export interface ForInNode extends IRNode {
+export interface ForInNode extends IRNodeBase {
   type: IRInst.FORIN;
   left: BlockNode;
   right: IRNode;
   body: IRNode;
 }
 
-export interface LoopNode extends IRNode {
+export interface LoopNode extends IRNodeBase {
   type: IRInst.LOOP;
   init: EmptyNode;
   test: IRNode;
@@ -141,8 +141,19 @@ export interface LoopNode extends IRNode {
   body: IRNode;
 }
 
-export interface DoWhileNode extends IRNode {
+export interface DoWhileNode extends IRNodeBase {
   type: IRInst.DO_WHILE;
   test: IRNode;
   body: IRNode;
 }
+
+export type IRNode =
+  | EmptyNode
+  | BlockNode
+  | SeqNode
+  | UpdatePropNode
+  | PropNode
+  | CondNode
+  | ForInNode
+  | LoopNode
+  | DoWhileNode;
