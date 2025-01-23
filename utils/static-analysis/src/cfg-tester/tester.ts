e75a2048f88a20aaffaa7f9922f7e7c1e4b252d9
seojinseojin <001106ksj@gmail.com>
2025-01-23 11:48:11 +0900

    [cfg][subgraph-mutable] enhance tester

--- utils/static-analysis/src/cfg-tester/tester.ts
+++ utils/static-analysis/src/cfg-tester/tester.ts
@@ -3,28 +3,37 @@ import { writeFile } from 'fs/promises';
 import cfg, { cfgToDot, generatePNG } from '../abstract/cfg';
 import { stringifyCFG } from '../compare/cfg-comparator';
 
-const code = `
+const codeMap = {
+  test1: `
     function a() {
-        if(a||_.b) c;
-        else _.d;
+      if(a||_.b) _.c;
+      else _.d;
     }
-`;
+`,
+  test2: `
+    function a(){
+      (a||_.b)?_.c:_.d;
+    }
+`,
+};
 
-const FILE_NAME = 'test';
-
-async function CFGtest(code: string) {
+async function CFGtest(code: string, filename: string) {
   const ast = acorn.parse(code, { ecmaVersion: 'latest' });
   const func_body = (ast.body[0] as any).body;
   const [cfgGraph] = cfg([{ id: '', name: '', params: [], body: func_body }]);
   const dot = await cfgToDot(cfgGraph.nodes);
-  await writeFile(`${FILE_NAME}.dot`, dot, 'utf-8');
-  await generatePNG(dot, `${FILE_NAME}.png`);
-  console.log('CFG generated ', `${FILE_NAME}.dot`, `${FILE_NAME}.png`);
+  await writeFile(`${filename}.dot`, dot, 'utf-8');
+  await generatePNG(dot, `${filename}.png`);
+  console.log('CFG generated ', `${filename}.dot`, `${filename}.png`);
   const cfgString = stringifyCFG(cfgGraph.nodes);
-  await writeFile(`${FILE_NAME}.txt`, cfgString, 'utf-8');
+  await writeFile(`${filename}.txt`, cfgString, 'utf-8');
   console.log(cfgString);
 }
 
-if (require.main === module) {
-  CFGtest(code).catch(console.error);
-}
+(async () => {
+  if (require.main === module) {
+    for (const [filename, code] of Object.entries(codeMap)) {
+      await CFGtest(code, filename).catch(console.error);
+    }
+  }
+})();
