c57c3cfbff4995b14dc054806aab6d1b687ad715
taxor03 <ryan040@korea.ac.kr>
2025-02-05 21:57:43 +0900

    [static] add hash weight related node size

--- utils/static-analysis/src/compare/hash-comparator.ts
+++ utils/static-analysis/src/compare/hash-comparator.ts
@@ -1,14 +1,19 @@
 import { CFGHash } from '../utils/types';
 
 function hash(h1: CFGHash[], h2: CFGHash[]): number {
-  const lib_hashset = new Set(
-    h1.map((h) => `${h.nodes.size},${h.hash}`) // Create string pairs
-  );
-  const code_hashset = new Set(
-    h2.map((h) => `${h.nodes.size},${h.hash}`) // Create string pairs
-  );
-  const includes = Array.from(lib_hashset).filter((h) => code_hashset.has(h));
-  console.log(includes.length, lib_hashset.size);
-  return includes.length / lib_hashset.size;
+  const lib_hashset = new Map(h1.map((h) => [`${h.hash}`, h.nodes.size]));
+  const code_hashset = new Map(h2.map((h) => [`${h.hash}`, h.nodes.size]));
+  let weightedIncludes = 0;
+  let totalWeight = 0;
+
+  lib_hashset.forEach((size, hash) => {
+    totalWeight += size;
+    if (code_hashset.has(hash)) {
+      weightedIncludes += size;
+    }
+  });
+
+  return weightedIncludes / totalWeight;
 }
+
 export default hash;
