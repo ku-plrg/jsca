b248690130d58a6a74d7ceb4f80518c20699ad5f
seojinseojin <001106ksj@gmail.com>
2025-02-07 18:29:20 +0900

    [evaluation] split parsing time and abstraction time

--- utils/static-analysis/src/lib_checker.ts
+++ utils/static-analysis/src/lib_checker.ts
@@ -6,14 +6,21 @@ import measureTime from './utils/timer';
 import { CFGHash } from './utils/types';
 
 function getHash(raw: string, logStr: string) {
-  const functions = measureTime(`extractFunctions in ${logStr}`, () =>
-    extractFunctions(raw)
+  const { value: functions, ms: parseMS } = measureTime(
+    `extractFunctions in ${logStr}`,
+    () => extractFunctions(raw)
   );
-  const cfgs = measureTime(`makeCFG in ${logStr}`, () => cfg(functions.value));
-  const hash = measureTime(`hashCFG in ${logStr}`, () =>
-    convertHash(cfgs.value)
+  const { value: cfgs, ms: abstMS } = measureTime(`makeCFG in ${logStr}`, () =>
+    cfg(functions)
   );
-  return hash.value;
+  const { value: hash, ms: hashMS } = measureTime(`hashCFG in ${logStr}`, () =>
+    convertHash(cfgs)
+  );
+  return {
+    hash,
+    parseMS,
+    abstMS: abstMS + hashMS,
+  };
 }
 
 function libraryChecker(
@@ -25,7 +32,7 @@ function libraryChecker(
 ): number {
   const libHash = cache[libName] || getHash(lib, 'library');
   if (!cache[libName]) cacheLib(libHash);
-  const codeHash = getHash(code, 'code');
+  const codeHash = getHash(code, 'code').hash;
 
   const result = measureTime('compare', () => compareHash(libHash, codeHash));
 
--- utils/static-analysis/src/web_analyzer.ts
+++ utils/static-analysis/src/web_analyzer.ts
@@ -6,20 +6,23 @@ import measureTime from './utils/timer';
 
 const dirPath = path.join(__dirname, '../../crawlers/website/data');
 const target: string[] = [
-  'bankofamerica.com', // lodash 4.17.21
-  'cnbc.com', // lodash 4.17.21
-  'ebay.com', // lodash 4.17.21
-  'hulu.com', // lodash 4.17.21
-  'discord.com', // lodash 4.17.19
-  'espn.com', // lodash 4.15.0
-  'marriott.com', // lodash 4.17.21
-  'mlb.com', // lodash 4.17.20
-  'netflix.com', // lodash 4.17.21
-  'nih.gov', // lodash 4.17.21
-  'roblox.com', // lodash 4.17.21
-  'speedtest.net', // lodash 4.17.21
+  // 'bankofamerica.com', // lodash 4.17.21
+  // 'cnbc.com', // lodash 4.17.21
+  // 'ebay.com', // lodash 4.17.21
+  // 'hulu.com', // lodash 4.17.21
+  // 'discord.com', // lodash 4.17.19
+  // 'espn.com', // lodash 4.15.0
+  // 'marriott.com', // lodash 4.17.21
+  // 'mlb.com', // lodash 4.17.20
+  // 'netflix.com', // lodash 4.17.21
+  // 'nih.gov', // lodash 4.17.21
+  // 'roblox.com', // lodash 4.17.21
+  // 'speedtest.net', // lodash 4.17.21
+  'gobstones.github.io',
 ];
-const libs = ['lodash_4.15.0_min', 'lodash_4.17.19_min', 'lodash_4.17.21_min'];
+const libs = [
+  'tween' /*'lodash_4.15.0_min', 'lodash_4.17.19_min', 'lodash_4.17.21_min'*/,
+];
 
 function getDirectDirs(dirPath: string) {
   return fs
@@ -59,36 +62,40 @@ function main() {
   const libHashes = libs.map((lib) => {
     const libPath = path.join(__dirname, `./target/initial/_score/${lib}.js`);
     const rawLib = fs.readFileSync(libPath, 'utf-8');
-    return getHash(rawLib, 'library');
+    return getHash(rawLib, 'library').hash;
   });
 
   getDirectDirs(dirPath)
     .filter((dir) => target.includes(dir))
     .forEach((dir) => {
       md += `## ${dir}\n`;
-      let table = `| File | ${libs.join(' | ')} | Abs-T | Comp-T |\n|-|${Array(
-        libs.length
-      )
+      let table = `| File | ${libs.join(
+        '|'
+      )} | Size | Parse-T | Abs-T | Comp-T |\n|${Array(libs.length + 5)
         .fill('-')
-        .join('|')}|-|-|\n`;
+        .join('|')}|\n`;
       const files = getAllFiles(path.join(dirPath, dir));
       const fileContents = files.map((file) => {
-        const content = fs.readFileSync(path.join(dirPath, dir, file), 'utf-8');
-        return { filename: file, content };
+        const filePath = path.join(dirPath, dir, file);
+        const content = fs.readFileSync(filePath, 'utf-8');
+        const stat = fs.statSync(filePath);
+        return { filename: file, content, size: stat.size };
       });
       const matches: { lib: string; code: string; score: number }[] = [];
       let totalAbsT = 0,
-        totalCompT = 0;
+        totalCompT = 0,
+        totalParseT = 0;
       const { ms } = measureTime(`compare ${dir}`, () => {
-        fileContents.forEach(({ filename, content }) => {
+        fileContents.forEach(({ filename, content, size }) => {
           try {
             table += `| ${FILE_TEMPLATE(filename, dir)} | `;
-            const { value: fileHash, ms: hashMs } = measureTime(
-              `abstract ${filename}`,
-              () => getHash(content, 'code')
-            );
-            totalAbsT += hashMs;
-
+            const {
+              hash: fileHash,
+              parseMS,
+              abstMS,
+            } = getHash(content, 'code');
+            totalParseT += parseMS;
+            totalAbsT += abstMS;
             const { ms: compMs } = measureTime(`compare ${filename}`, () =>
               libHashes.forEach((libHash, lbiIdx) => {
                 const score = compareHash(libHash, fileHash);
@@ -102,7 +109,7 @@ function main() {
               })
             );
             totalCompT += compMs;
-            table += ` ${hashMs} | ${compMs} |`;
+            table += ` ${size}byte | ${parseMS}ms | ${abstMS}ms | ${compMs}ms |\n`;
           } catch (e) {
             console.log(e);
             table += '\n';
@@ -111,7 +118,7 @@ function main() {
       });
 
       md += `Time: ${ms}ms\n`;
-      md += `|Abstraction Time|Compare Time|\n|-|-|\n|${totalAbsT}|${totalCompT}|\n`;
+      md += `|Parse Time|Abstraction Time|Compare Time|\n|-|-|-|\n|${totalParseT}ms|${totalAbsT}ms|${totalCompT}ms|\n`;
       const matchTable = `| Library | File | Similarity |\n|-|-|-|\n${matches
         .sort((a, b) => b.score - a.score)
         .map(
